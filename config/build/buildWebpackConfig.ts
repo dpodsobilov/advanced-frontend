3b5ed8e043967c443d0d309a75641c06731e01b1
Denis Podsobilov <dpodsobilov29@gmail.com>
2025-01-02 20:51:56 +0400

    Lessons 9-10 - Restructured in accordance with feature-sliced design

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
-import path from "path";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
@@ -23,7 +22,7 @@ export function buildWebpackConfig(
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? "inline-source-map" : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
