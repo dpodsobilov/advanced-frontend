de7d7a3e46e45a86cca70ef9780b606e40a3095d
Denis Podsobilov <dpodsobilov29@gmail.com>
2025-01-02 23:11:58 +0400

    Chapter 2, Lesson 11-12 - Router config, navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,17 @@
-import { Suspense } from "react";
 import "./styles/index.scss";
-import { Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/NavBar";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
