91fb29d2b2103618b3cd76078994a87824287721
Denis Podsobilov <dpodsobilov29@gmail.com>
2025-01-03 23:13:45 +0400

    Lesson 19 - Added stylelint and i18next-eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:i18next/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -12,7 +17,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'i18next'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-indent': [2, 2],
@@ -33,6 +38,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'warn',
     'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['warn', { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
